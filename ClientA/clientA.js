32ec5ff3285317c08564acbd865dd7666841de98
Ganesh Kanna MC <emcydeveloper@gmail.com>
2022-03-13 17:14:36 +0530

    WebRTCLearning_LocalMessagingServices

---
+++ ClientA/clientA.js
@@ -0,0 +1,95 @@
+// client_A();
+document.getElementById("openClientB").addEventListener("click", client_A);
+
+function client_A() {
+  /*
+//you can specify a STUN server here
+
+const iceConfiguration = { }
+iceConfiguration.iceServers = [];
+//turn server
+iceConfiguration.iceServers.push({
+                urls: 'turn:my-turn-server.mycompany.com:19403',
+                username: 'optional-username',
+                credentials: 'auth-token'
+            })
+//stun  server
+iceConfiguration.iceServers.push({
+                urls: 'stun:stun1.l.google.com:19302' 
+            })    
+
+const localConnection = new RTCPeerConnection(iceConfiguration)
+
+
+*/
+
+  const localConnection = new RTCPeerConnection();
+
+  localConnection.onicecandidate = (e) => {
+    console.log(" NEW ice candidnat!! on localconnection reprinting SDP ");
+    const offer = JSON.stringify(localConnection.localDescription);
+    console.log("I am from client A - " + offer);
+    document.getElementById("clientA_Offer").value = offer;
+    // document.getElementById("openClientB").addEventListener("click", );
+    // document.getElementById("ConnectClientB").addEventListener("click", function () {
+    //   testJS(offer);
+    // });
+
+    //  document.getElementById("ConnectClientB").addEventListener("click", function (){ConnectToClientB(offer)});
+  };
+
+  //ConnectToB
+  document.getElementById("ConnectToB").addEventListener("click", function () {
+    // alert("C");
+    //clientB_Answer
+    const answer = document.getElementById("clientB_Answer").value;
+    console.log(answer);
+
+    const sendAnswer = { answer };
+    console.log(JSON.parse(sendAnswer.answer));
+    console.log(typeof JSON.parse(sendAnswer.answer));
+
+    localConnection.setRemoteDescription(JSON.parse(sendAnswer.answer));
+
+    //localConnection.setRemoteDescription(answer)
+  });
+
+  const sendChannel = localConnection.createDataChannel("sendChannel");
+  sendChannel.onmessage = (e) => {
+    console.log("messsage received!!!" + e.data);
+    document.getElementById(
+      "messages"
+    ).innerHTML = `Message from client B - <b> ${e.data} </b>`;
+  };
+  sendChannel.onopen = (e) => {
+    console.log("Connection A is open!!!!");
+    alert("Connection A is open and ready to communicate with B!!!!");
+  };
+  sendChannel.onclose = (e) => {
+    console.log("Connection A is closed!!!!!!");
+    alert("Connection A is closed!!!!!!");
+  };
+
+  localConnection
+    .createOffer()
+    .then((o) => localConnection.setLocalDescription(o));
+
+  document
+    .getElementById("sendmessagetoB")
+    .addEventListener("click", function () {
+      let messsage = document.getElementById("ClientAMessage").value;
+      sendChannel.send(messsage);
+    });
+}
+
+// // document.getElementById("openClientB").addEventListener("click", client_A);
+// function ConnectToClientB(offer) {
+//   //let aa = offer.replace(/ /g,"||");
+//   const urlParams = new URLSearchParams(`?val=${offer}`);
+
+//     for (const [key, value] of urlParams) {
+//       console.log(`testingggggggaaaaaaaaaaaaaaa - ${value}`);
+//   }
+
+//   //window.location.href = "clientB.html" + offer;
+// }
---
+++ ClientB/clientB.js
@@ -0,0 +1,87 @@
+// function test(){
+//   alert("am called")
+//   const urlParams = new URLSearchParams(location.search);
+// for (const [key, value] of urlParams) {
+//   console.log(`testinggggggg - ${value}`);
+// }}
+
+// function getParameterByName(name, url = window.location.href) {
+//   name = name.replace(/[\[\]]/g, '\\$&');
+//   var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+//       results = regex.exec(url);
+//   if (!results) return null;
+//   if (!results[2]) return '';
+//   return decodeURIComponent(results[2].replace(/\+/g, ' '));
+// }
+
+function myFunction() {
+  var val = document.getElementById("myText").value;
+  const sendOffer = { val };
+  client_B(JSON.parse(sendOffer.val));
+}
+
+// document.getElementById("getingpara").addEventListener("click", function () {
+//   let offer = document.getElementById("Response").value;
+//   console.log(offer);
+//   document.getElementById("demo").innerHTML = offer;
+//   //client_B(offer);
+// });
+
+//document.getElementById("getingpara").addEventListener("click",client_B);
+
+async function client_B(offer) {
+  //getOffer
+
+  //const offer = {"type":"offer","sdp":"v=0\r\no=- 2794800499485477739 2 IN IP4 127.0.0.1\r\ns=-\r\nt=0 0\r\na=group:BUNDLE 0\r\na=extmap-allow-mixed\r\na=msid-semantic: WMS\r\nm=application 56769 UDP/DTLS/SCTP webrtc-datachannel\r\nc=IN IP4 192.168.0.101\r\na=candidate:3350409123 1 udp 2122260223 192.168.0.101 56769 typ host generation 0 network-id 1\r\na=candidate:4214507315 1 udp 2122194687 192.168.149.1 56770 typ host generation 0 network-id 2\r\na=candidate:1773322460 1 udp 2122129151 192.168.126.1 56771 typ host generation 0 network-id 3\r\na=candidate:2301678419 1 tcp 1518280447 192.168.0.101 9 typ host tcptype active generation 0 network-id 1\r\na=candidate:3048717251 1 tcp 1518214911 192.168.149.1 9 typ host tcptype active generation 0 network-id 2\r\na=candidate:657538092 1 tcp 1518149375 192.168.126.1 9 typ host tcptype active generation 0 network-id 3\r\na=ice-ufrag:mzey\r\na=ice-pwd:DFZjQwnlul1QN5mmgJMYz13Z\r\na=ice-options:trickle\r\na=fingerprint:sha-256 BD:2C:61:4A:56:B9:7A:42:FA:55:10:1E:1D:DF:2D:37:F3:9D:D1:04:FA:2F:AD:49:92:FB:8C:4E:66:E3:7F:67\r\na=setup:actpass\r\na=mid:0\r\na=sctp-port:5000\r\na=max-message-size:262144\r\n"};
+  //set offer const offer = ...
+
+  console.log(offer);
+  console.log(typeof offer);
+  let answer;
+  const remoteConnection = new RTCPeerConnection();
+
+  remoteConnection.onicecandidate = (e) => {
+    console.log(" NEW ice candidnat!! on localconnection reprinting SDP ");
+    console.log(JSON.stringify(remoteConnection.localDescription));
+  };
+
+  remoteConnection.ondatachannel = (e) => {
+    const receiveChannel = e.channel;
+    receiveChannel.onmessage = (e) => {
+      console.log("messsage received!!!" + e.data);
+      document.getElementById(
+        "messages"
+      ).innerHTML = `Message from Client A - <b> ${e.data} </b>`;
+    };
+    receiveChannel.onopen = (e) => {
+      console.log("Connection B is Open!!!!");
+      alert("Connection B is Open and ready to communicate with A!!!!");
+    };
+    receiveChannel.onclose = (e) => {
+      console.log("Connection B is Closed!!!!!!");
+      alert("Connection B is Closed!!!!!!");
+    };
+    remoteConnection.channel = receiveChannel;
+
+    document
+      .getElementById("sendmessagetoA")
+      .addEventListener("click", function () {
+        let message = document.getElementById("ClientBMessage").value;
+        receiveChannel.send(message);
+      });
+  };
+
+  remoteConnection.setRemoteDescription(offer).then((a) => console.log("done"));
+
+  //create answer
+  await remoteConnection
+    .createAnswer()
+    .then((a) => remoteConnection.setLocalDescription(a))
+    .then((a) => {
+      answer = JSON.stringify(remoteConnection.localDescription);
+      console.log(`I am from Client B - ${answer}`);
+      document.getElementById("ClientBAnswer").value = answer;
+    });
+
+  //send the anser to the client
+}
